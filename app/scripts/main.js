d69a359bf554ce4e01b479c4917ddc9e706084d3
Matt Johnson <matt@mattjohnson.io>
2014-11-27 15:33:44 +0000

    Preview video prior to upload.
    Currently without audio.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,7 @@
 var Emitter   = require('wildemitter'),
     $         = require('jquery'),
-    RecordRTC = require('recordrtc');
-
-var $recordVideoButton = $('.js-record-video'),
-    Owl = {};
+    RecordRTC = require('recordrtc'),
+    Owl       = {};
 
 navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.webkitGetUserMedia ||
@@ -14,7 +12,9 @@ Owl.recorder = (function(rtc) {
   var module = {},
       stream,
       audioRecorder,
-      videoRecorder;
+      videoRecorder,
+      audioURL,
+      videoURL;
 
   $.extend(module, new Emitter());
 
@@ -39,6 +39,8 @@ Owl.recorder = (function(rtc) {
     } else {
       this.start();
     }
+
+    return false;
   };
 
   module.start = function() {
@@ -54,13 +56,38 @@ Owl.recorder = (function(rtc) {
 
     audioRecorder.getDataURL(function(audioDataURL) {
       videoRecorder.getDataURL(function(videoDataURL) {
-        this.emit('stop', {audio: audioDataURL, video: videoDataURL});
+        audioURL = audioDataURL;
+        videoURL = videoDataURL;
+
+        this.emit('stop');
       }.bind(this));
     }.bind(this));
   };
 
+  module.getAudioURL = function() {
+    return audioURL;
+  };
+
+  module.getVideoURL = function() {
+    return videoURL;
+  };
+
   return module;
 })(RecordRTC);
+
+Owl.playback = (function() {
+  var module = {};
+
+  $.extend(module, new Emitter());
+
+  module.loadMedia = function(audioDataURL, videoDataURL, $playbackSection) {
+    $playbackSection.find('video')[0].src = videoDataURL;
+    $playbackSection.find('audio')[0].src = audioDataURL;
+    this.emit('load');
+  };
+
+  return module;
+})();
 
 Owl.uploader = (function($) {
   var module = {};
@@ -75,13 +102,16 @@ Owl.uploader = (function($) {
     module.emit('error', arguments);
   }
 
-  module.uploadMedia = function(media) {
+  module.uploadMedia = function(audioDataURL, videoDataURL) {
     var xhr;
 
     xhr = $.ajax({
       url: 'http://192.168.1.90:8080/videos/',
       type: 'POST',
-      data: media
+      data: {
+        audio: audioDataURL,
+        video: videoDataURL
+      }
     });
 
     xhr.done(onUploadMedia);
@@ -93,12 +123,65 @@ Owl.uploader = (function($) {
 
 
 $(function() {
+  var $recorderSection      = $('.recorder'),
+      $playbackSection      = $('.playback'),
+      $recorderSectionTitle = $recorderSection.find('h1'),
+      $playbackSectionTitle = $playbackSection.find('h1'),
+      $recordVideoButton    = $('.js-record-video'),
+      $cancelVideoButton    = $('.js-cancel-video'),
+      $uploadVideoButton    = $('.js-upload-video'),
+      recordIconClass       = 'icon-videocam',
+      stopIconClass         = 'icon-stop';
+
+  function makeVisible($element) {
+    $element.addClass('visible');
+  }
+
+  function makeInvisible($element) {
+    $element.removeClass('visible');
+  }
+
+  /**
+  Recorder UI changes
+  */
+
   Owl.recorder.on('start', function() {
-    console.log('started');
+    $recorderSectionTitle.text('Recording…');
+    $recordVideoButton.find('i').removeClass(recordIconClass).addClass(stopIconClass);
   });
-  Owl.recorder.on('stop', Owl.uploader.uploadMedia.bind(Owl.uploader));
+
+  Owl.recorder.on('stop', function() {
+    Owl.playback.loadMedia(Owl.recorder.getAudioURL(),
+                           Owl.recorder.getVideoURL(),
+                           $playbackSection);
+
+    makeInvisible($recorderSection);
+    makeVisible($playbackSection);
+
+    $recorderSectionTitle.text('Record your story');
+    $recordVideoButton.find('i').removeClass(stopIconClass).addClass(recordIconClass);
+  });
+
   Owl.uploader.on('error', function(errorArgs) {
     console.log('upload error', errorArgs);
   });
+
+  /**
+  UI action bindings
+  */
+
   $recordVideoButton.on('click', Owl.recorder.toggleRecording.bind(Owl.recorder));
+
+  $cancelVideoButton.on('click', function() {
+    makeInvisible($playbackSection);
+    makeVisible($recorderSection);
+  });
+
+  $uploadVideoButton.on('click', function() {
+    makeInvisible($playbackSection.find('.playback-controls'));
+    makeVisible($playbackSection.find('.playback-uploading'));
+
+    Owl.uploader.uploadMedia(Owl.recorder.getAudioURL,
+                             Owl.recorder.getVideoURL);
+  });
 });
